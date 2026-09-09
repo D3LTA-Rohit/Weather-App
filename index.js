1264861b258a4f82166df32349cf69247d02ebdb
Rohit Kumar <tanwarrohit418@gmail.com>
2026-09-09 11:08:21 +0530

    UPDATE index.js with event listeners, main(), getWeatherData() and  displayError() functions.

--- index.js
+++ index.js
@@ -22,3 +22,60 @@ function changeTheme() {
         darkMode = false;
     }
 }
+
+const inputCity = document.getElementById("inputCity");
+const searchButton = document.getElementById("searchButton");
+const output = document.getElementById("output");
+const apiKey = "c04a3191655e34ec6392a2bda8d309c5";
+
+inputCity.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        main();
+    }
+});
+
+searchButton.addEventListener("click", main);
+
+async function main() {
+    const city = inputCity.value.trim().toLowerCase();
+    if (!city) {
+        displayError("Please enter a city!!");
+    } else {
+        
+        try {
+            const weatherData = await getWeatherData(city);
+            displayWeatherData(weatherData);
+        } catch (error) {
+            console.error(error);
+            displayError(error);
+        }
+    }
+}
+
+async function getWeatherData(city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+        console.error("Could not fetch whether data.");
+        displayError("City not found.");
+    }
+    return await response.json();
+}
+
+function displayWeatherData(data) {
+
+}
+
+function getWeatherEmoji(weatherID) {
+
+}
+
+function displayError(message) {
+    const error = document.createElement("h5"); //h5 element with id = "error" created
+    error.textContent = message; //text content changed
+    error.classList.add("error"); //.error class added, so styling can be applied acc. to CSS
+
+    //output.textContent = "";
+    output.appendChild(error);
+    output.style.display = "flex";
+}
